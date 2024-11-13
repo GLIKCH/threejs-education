195c49f2f98a95d48dff6021664f04086dbf2f55
glikch <sightlinepcs@outlook.com>
2024-11-12 23:31:26 -0500

    Commit01-2024-11-12

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-console.log('Robot Bobby was here');
+import * as THREE from "three";
+
+// Creating the Renderer
+const w = window.innerWidth;
+const h = window.innerHeight;
+const renderer = new THREE.WebGLRenderer({antialias: true});
+renderer.setSize(w, h);
+document.body.appendChild(renderer.domElement);
+
+// Camera Constant Parameters
+const fov = 75;
+const aspect =  w / h;
+const near = 0.1;
+const far = 10;
+
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+camera.position.z = 2;
+const scene = new THREE.Scene();
+
+// Parameters (Size, Detail)
+const geo = new THREE.IcosahedronGeometry(1.0, 2);
+// Basic Hex Color
+const mat = new THREE.MeshStandardMaterial({
+    color: 0xffffff,
+    flatShading: true
+});
+const mesh = new THREE.Mesh(geo, mat);
+scene.add(mesh);
+
+const wireMat = new THREE.MeshBasicMaterial({
+    color: 0xFE4527,
+    wireframe: true
+});
+
+const wireMesh = new THREE.Mesh(geo, wireMat);
+scene.add(wireMesh);
+
+const hemiLight = new THREE.HemisphereLight(0xffffff, 0x00000);
+scene.add(hemiLight);
+
+function animate(){
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
+
+renderer.render(scene, camera);
+
