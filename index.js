cece1e0225484e55d71b6e5760de3f3bcb2b94b8
glikch <sightlinepcs@outlook.com>
2024-11-15 10:56:01 -0500

    commit02-2024-11-15

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+// Application Import Dependencies
+
 import * as THREE from "three";
+import { OrbitControls } from "jsm/controls/OrbitControls.js";
 
 // Creating the Renderer
+// Assigning Window Size Scale
 const w = window.innerWidth;
 const h = window.innerHeight;
+
+// Renderer Begin
 const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize(w, h);
 document.body.appendChild(renderer.domElement);
@@ -13,12 +19,19 @@ const aspect =  w / h;
 const near = 0.1;
 const far = 10;
 
+// Perspective Camera
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera.position.z = 2;
+camera.position.z = 4;
 const scene = new THREE.Scene();
 
+// Mouse Orbit Controls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.03;
+
+// Enter Geometry and Objects
 // Parameters (Size, Detail)
-const geo = new THREE.IcosahedronGeometry(1.0, 2);
+const geo = new THREE.IcosahedronGeometry(0.40, 10);
 // Basic Hex Color
 const mat = new THREE.MeshStandardMaterial({
     color: 0xffffff,
@@ -32,16 +45,32 @@ const wireMat = new THREE.MeshBasicMaterial({
     wireframe: true
 });
 
+const wireMat2 = new THREE.MeshBasicMaterial({
+    color: 0x00ff90,
+    wireframe: true
+});
+
 const wireMesh = new THREE.Mesh(geo, wireMat);
-scene.add(wireMesh);
+wireMesh.scale.setScalar(4.001);
+mesh.add(wireMesh);
+
+const wireMesh2 = new THREE.Mesh(geo, wireMat2);
+wireMesh2.scale.setScalar(5.001);
+mesh.add(wireMesh2);
 
 const hemiLight = new THREE.HemisphereLight(0xffffff, 0x00000);
 scene.add(hemiLight);
 
-function animate(){
+// Iso Animation (Rotation)
+function animate(t = 0){
     requestAnimationFrame(animate);
+    mesh.rotation.y = t * 0.0000;
+    mesh.rotation.z = t * 0.0000;
     renderer.render(scene, camera);
+    controls.update();
 }
+
+animate();
 
 renderer.render(scene, camera);
 
